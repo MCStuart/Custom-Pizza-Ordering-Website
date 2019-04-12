accdd2a44423b6a8117799b3ef6d6caa2bea322c
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-04-12 10:31:43 -0700

    add functional buttons to navigate through pizza order options and display final order. No back end logic yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,26 @@
 // Business Logic
+// Starts new pizza order
 function PizzaOrder() {
   this.pizzas = []
 };
-
-function Pizza() {
+// Starts new individual pizza
+function Pizza(topping, size) {
   this.toppings = [],
   this.size = size,
   this.price = 10
 };
 
-
-
 // User Interface Logic
 $(document).ready(function() {
-
+  var pizzaOrder = new PizzaOrder();
+  $("button#pizzaButton").click(function(event) {
+    event.preventDefault();
+    $("button#pizzaButton").hide();
+    $("form.pizzaOptions").show();
+  });
+  $("form.pizzaOptions").submit(function(event) {
+    event.preventDefault();
+    $("form.pizzaOptions").hide();
+    $("div.checkout").show();
+  });
 });
