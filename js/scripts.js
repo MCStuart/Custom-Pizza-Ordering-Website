d1794d1007225975a73fbe14bfa14ffa9633653f
Stuart McKay <stuart51994gmail.comgit config --global user.email stuart51994@gmail.com>
2019-04-12 09:55:25 -0700

    add html pizza size and toppings options for javascript BDD

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// Business Logic
+function PizzaOrder() {
+  this.pizzas = []
+};
+
+function Pizza() {
+  this.toppings = [],
+  this.size = size,
+  this.price = 10
+};
+
+
+
+// User Interface Logic
+$(document).ready(function() {
+
+});
